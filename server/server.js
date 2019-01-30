4db6cddd6def70f19dc2ff75083672cb24bfdb87
fredtsui <yizhi.tsui@gmail.com>
2019-01-29 23:39:09 -0500

    mongodb init
    connect to db

--- server/server.js
+++ server/server.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoClient = require("mongodb").MongoClient;
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const url = 'mongodb://localhost:27017';
+const eventsDB = "events";
+const bookingsDB = "bookings";
+
+const client = new mongoClient(url);
+
+client.connect((err) => {
+    if(err === null){
+        console.log("connected to mongodb!");
+        const events = client.db(eventsDB);
+        const bookings = client.db(bookingsDB);
+    }
+    client.close();
+})
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
@@ -14,5 +30,6 @@ app.post('/api/world', (req, res) => {
     `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
 });
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
